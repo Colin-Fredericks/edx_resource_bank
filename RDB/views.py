2d518b88c546a0c5ad7efaf8808158cd3ee39d86
Colin Fredericks <cfred@mit.edu>
2013-07-01 16:57:58 -0400

    Fixing views. Attempting to add css.
    We can now see the analytics values!

--- RDB/views.py
+++ RDB/views.py
@@ -3,7 +3,7 @@
 
 from django.http import Http404
 from django.shortcuts import render
-from RDB.models import Resource
+from RDB.models import Resource, Analytic_Value
 
 # def index(request):
 #	 latest_resource_list = Resource.objects.order_by('-creation_date')[:5]
@@ -13,17 +13,22 @@ from RDB.models import Resource
 
 
 def index(request):
+	resource_list = Resource.objects.all().order_by('id')
 	latest_resource_list = Resource.objects.all().order_by('-creation_date')[:5]
-	context = {'latest_resource_list': latest_resource_list,}
+	context = {
+		'latest_resource_list': latest_resource_list, 
+		'resource_list': resource_list,	
+	}
 	return render(request,'RDB/index.html',context)
 
 
 def detail(request, resource_id):
 	try:
-		resource = Resource.objects.get(pk=resource_id)
+		res = Resource.objects.get(pk=resource_id)
 	except Resource.DoesNotExist:
 		raise Http404
-	return render(request, 'RDB/detail.html', {'resource': resource})
+	analytic_values = Analytic_Value.objects.filter(resource=res)
+	return render(request, 'RDB/detail.html', {'resource': res, 'analytic_values': analytic_values,})
 
 
 def results(request, resource_id):
