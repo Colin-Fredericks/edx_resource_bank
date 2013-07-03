ae7f633576b6e95ea1d56e18dd49d91b0436f6b1
Colin Fredericks <cfred@mit.edu>
2013-07-03 16:54:30 -0400

    Progress on the CSV reader

--- utils/read_in_file.py
+++ utils/read_in_file.py
@@ -1,33 +1,81 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import MySQLdb
+
+""" Example of SQL stuff
+		# Use all the SQL you like
+		cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+
+		# print all the first cell of all the rows
+		for row in cur.fetchall() :
+		print row[0]
+
+"""
+
+
+
+
 # Function that converts things
-# Take in a filename from the command line
+def main(argv):
 
-# open the file
-spreadsheet = open('name_of_file', 'r')
+	# Check for correct usage
+	if not argv:
+		sys.exit("Please specify an input file in csv format.")
+	if len(argv) == 1:
+		sys.exit("Usage: python read_in_file.py whatever.csv")
+	if argv[1] == "-h":
+		sys.exit("Usage: python read_in_file.py whatever.csv")
 
-# Loopy-loo: until we're done with the file
+	# Take in a filename from the command line
+	filename = str(sys.argv[1])
+
+	# open the file
+	with open(filename, 'rb') as csvfile:
+
+		"""		# Connect to the database
+				db = MySQLdb.connect(host="localhost",
+								user="resource_mangler",
+								passwd="1l0v3dat3r",
+								db="edxresources")
+
+				# Create a Cursor object with which to execute queries
+				cur = db.cursor() 
+		"""
+		# Create a CSV reader
+		spreadsheet = csv.reader(csvfile)
+
+		# Loopy-loo: until we're done with the file...
+		for row in spreadsheet:
 	
-	# read a line of the file
+			# Take the second column and make a Collection out of it, if one doesn't already exist. 
+			# Add this resource to the collection from column 2.
+			# currently testing by printing it out
+			print row[2]
 
-	# break it up by CSV pieces
+			# Put the third column into the "name" field
 
-	# Ignore first column (index)
+			# If the fourth column has the word "problem", mark resource_type = problem
 
-	# Take the second column and make a Collection out of it, if one doesn't already exist. 
-	# Add this resource to the collection from column 2.
+			# Skip column five
+			# Check to see if Columns 6, 7, 8 already exist as learning objectives.
+				# if not, create them.
+			# Add this resource to the learning objectives for columns 6, 7, 8
 
-	# Put the third column into the "name" field
+			# Put column 9 into the "description" field
 
-	# If the fourth column has the word "problem", mark resource_type = problem
+			# Put column 10 in as a custom text field named "LC SYMB"
+			
+			# next line -- handled automatically by the "for reader in row" statement
 
-	# Skip column five
-	# Check to see if Columns 6, 7, 8 already exist as learning objectives.
-		# if not, create them.
-	# Add this resource to the learning objectives for columns 6, 7, 8
+		# End loopy-loo
 
-	# Put column 9 into the "description" field
+	# Close the file -- done automatically via the "with" command
 
-	# Put column 10 in as a custom text field named "LC SYMB"
+# Done with main
 
-	# End loopy-loo
 
-# Close the file
+# Helps with file execution
+if __name__ == "__main__":
+   main(sys.argv[0:])
