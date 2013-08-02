2d51f84ae7e7e1710b7c06af09e3e7ab03e9e0a4
Colin Fredericks <cfred@mit.edu>
2013-08-02 18:12:17 -0400

    Refactoring some things, flailing on others.

--- utils/toy_course/read_in_course.py
+++ utils/toy_course/read_in_course.py
@@ -44,15 +44,17 @@ def main(argv):
 	"""
 	cur = ""
 	
-	collection_collection = [filename, ]
+	collection_list = [filename]
+	depth = 0
 		
-	BigLoop(filename, "", "", collection_collection, 0, cur)
+	BigLoop(filename, "", "", collection_list, depth, cur)
 
 
 # This is the recursive function that does most of our work.
-def BigLoop(filepath, tag_type, display_name, cc, depth, cur):
+def BigLoop(filepath, tag_type, display_name, collection_list, depth, cur):
 
-	collection_collection = cc[:]
+	# Note: The collection_list is being passed by REFERENCE, not by value.
+	# We need to manually remove entries.
 
 	if depth > 10:
 		sys.exit("Potential infinite loop detected. Exiting. Check for files that reference themselves?")
@@ -60,99 +62,107 @@ def BigLoop(filepath, tag_type, display_name, cc, depth, cur):
 	# trackers so that we can give some output
 	# I just realized these will all need to get passed if we're going to actually use them.
 	# added_resources = 0
-	# added_collections = 0
 	# linked_objectives = 0 
+	added_collections = 0
 	filepaths_found = 0
+	
 
 	# open the file
 	try:
 		with open(filepath, 'rbU') as xmlfile:
 
+			"""  Commenting out non-working section for clean upload.
+			# Use the first line of all but the first file to double-check the display_name.
+			if depth > 0:
+				firstline = xmlfile.readline()
+				if 'display_name' in firstline:
+
+					# If the display name doesn't match the last item in the collection list, update the list.
+					d_n = re.search('display_name="(.*?)"', firstline)
+					if d_n != collection_list[len(collection_list)-1]:
+						collection_list[len(collection_list)-1] = d_n.group(1)
+			"""
 
 			# For every line in this file:
 			for line in xmlfile:
-			
-				"""    Still working on this section. Commented out for clean upload.
-				# I'm concerned that we're only keeping one collection.
-				# Resources should belong to multiple collections, and I think this only adds it to the last one found.
-				# If this line has a <chapter> or <sequential> or <vertical> tag...
-				if '<chapter' in line or '<sequential' in line or '<vertical' in line:
 
-					# Escape the display_name and dump it in current_collection.
-					dn = re.search('display_name="(.*?)"', line)
-					if dn:
-						if re.escape(dn.group(1)) not in collection_collection:
-							collection_collection = collection_collection + [re.escape(dn.group(1))]
+				"""  More non-working stuff.
+				# If this line points to a container:
+				if '<course' in line or '<chapter' in line or '<sequential' in line or '<vertical' in line:
+
+					# Get the display name from this line so we can pass it to the next file.
+					if 'display_name' in line:
+						d_n = re.search('display_name="(.*?)"', line)
+						collection_list += [d_n.group(1)]
 					else:
-						if re.escape(xmlfile.name) not in collection_collection:
-							collection_collection = collection_collection + [re.escape(xmlfile.name)]
-					
-					print collection_collection
+						# If there's no display name for this element, use the linked filepath instead.
+						# Will probably want to do some processing on this to fix up or strip down the path.
+						if "filename" in line:
+							f_n = re.search('filename="(.*?)"', line)
+							collection_list += [f_n.group(1)]
+						elif "url_name" in line:
+							u_n = re.search('url_name="(.*?)"', line)
+							collection_list += [u_n.group(1)]
+						else:
+							collection_list += ['unknown collection']
 				"""
-
-
+					
 				# If the tag on this line closes on the same line, attempt to open the file it links to and traverse the file tree.
 				# If it's not self-closing, it doesn't actually link to a file. Move on.
 
-				if re.search('/>',line):
+				if '/>' in line:
 
-					# If this line has a filename="" attribute, use that and go there:
-					if re.search('filename="(.*?)"', line):
-			
-						# Get the filepath, tag type, and the display_name from the same line.
-						filepath = re.search('filename="(.*?)"', line).group(1)
+					# If this line has a filename or url_name attribute, use that and go there:
+					if 'filename' in line or 'url_name' in line:
+
 						tag_type = re.search('<(\S+?) ',line).group(1)
-						dn = re.search('display_name="(.*?)"', line)
-						if dn:
-							display_name = dn.group(1)
+
+						if 'display_name' in line:
+							dn = re.search('display_name="(.*?)"', line)
 						else:
 							display_name = ''
 
-						# Correct the filename - add folder and .xml if needed.
-						if '<problem ' in line:
-							if filepath.find("problems/") != 0:
-								filepath = 'problems/' + filepath   # Note the s.
-							filepath = filepath + '.xml'
-						else:
-							filepath = FixPath(filepath, line)
-					
-						# Run this function with those three things as arguments.
-						# print "opening filename " + filepath
-						BigLoop(filepath, tag_type, display_name, collection_collection, depth+1, cur)
+						if 'filename' in line:
+
+							# Get the filepath
+							filepath = re.search('filename="(.*?)"', line).group(1)
+
+							# Correct the filename - add folder and .xml if needed.
+							if '<problem ' in line:
+								if filepath.find("problems/") != 0:
+									filepath = 'problems/' + filepath   # Note the s.
+								filepath = filepath + '.xml'
+							else:
+								filepath = FixPath(filepath, line)
+
+						if 'url_name' in line:
+
+							# Get the filepath
+							filepath = re.search('url_name="(.*?)"', line).group(1)
+
+							# Correct the filepath. Swap out colons, add folder and .xml if needed.
+							filepath = filepath.replace(':','/')
+							if '<problem ' in line:
+								if filepath.find("problem/") != 0:
+									filepath = 'problem/' + filepath   # Note the lack of s.
+								filepath = filepath + '.xml'
+							else:
+								filepath = FixPath(filepath, line)
+
+						# Recursion happens here.
+						BigLoop(filepath, tag_type, display_name, collection_list, depth+1, cur)
+
+						# Pop the last collection from the list, unless it's empty. 
+						# Which it should never be after coming back from recursing.
+						if len(collection_list) > 0:
+							collection_list.pop()
 						filepaths_found += 1
-				
-
-					# If this line has a url_name="" attribute, things work slightly differently:
-					elif re.search('url_name="(.+?)"',line):
-
-						# Get the filepath, tag type, and the display_name from the same line.
-						filepath = re.search('url_name="(.*?)"', line).group(1)
-						tag_type = re.search('<(\S+?) ',line).group(1)
-						dn = re.search('display_name="(.*?)"', line)
-						if dn:
-							display_name = dn.group(1)
-						else:
-							display_name = ''
-					
-						# Correct the filepath. Swap out colons, add folder and .xml if needed.
-						filepath = filepath.replace(':','/')
-						if '<problem ' in line:
-							if filepath.find("problem/") != 0:
-								filepath = 'problem/' + filepath   # Note the lack of s.
-							filepath = filepath + '.xml'
-						else:
-							filepath = FixPath(filepath, line)
-				
-						# Run this function with those three things as arguments.
-						# print "opening urlname " + filepath
-						BigLoop(filepath, tag_type, display_name, collection_collection, depth+1, cur)
-						filepaths_found += 1
-
+						
 				# Move to next line (done automatically by the for loop)
 
 			# If there are no self-closing tags with filepaths found in this whole file:
 			if filepaths_found == 0:
-
+			
 				# We're going to INSERT a new resource into the database.
 							
 				# It is possible for the display_name to be blank. If not:
@@ -160,9 +170,10 @@ def BigLoop(filepath, tag_type, display_name, cc, depth, cur):
 					# Take the current display_name, escape it, and dump it into the "name" field.
 					name = re.escape(display_name)
 				else:
-					# Auto-name the resource in some sort of reasonable and hopefully unique manner.
+					# If there's no display_name, auto-name the resource in some sort of reasonable and hopefully unique manner.
 					name = re.escape(xmlfile.name)
 
+				# Just testing
 				print name
 
 				# Take the entire text of this file, escape it, and dump it into the "text" field.
@@ -198,18 +209,108 @@ def BigLoop(filepath, tag_type, display_name, cc, depth, cur):
 					elif re.search('<custom', text):
 						problem_type = 'custom'
 					else:
+						# If none of these tags are found, go back and set the resource_type to "other"
 						problem_type = 'other'
 				else:
 					problem_type = 'not_a_problem'
 
-					# If no problem_type found, go back and set the resource_type to "other"
-				# Other required items that we will also need:
-					# Learning Objectives!!
-					# Description
-				# Run the MySQL INSERT command.
-				# Link this resource to the current_collection, creating the collection if necessary.
-					# (The Collection_Creator function does this just how I want.)
+				""" Commented out for testing. This is the SQL insert block.
 
+				# Placeholder values.
+				# Note that we'll need to link learning objectives later.
+				description = ''
+				resource_file = ''
+				grade_level = ''
+				intended_use = ''
+				license = ''
+				license_link = ''
+				license_other_notes = ''
+				source = ''
+				language = ''
+				author = ''
+				comments = ''
+				creation_date = '2001-01-01'
+				solutions_hints_etc = ''
+
+
+				# Assemble the MySQL INSERT command.
+				sql_start = "SELECT * FROM RDB_resource  WHERE"
+			
+				sql_left = "(name, "
+				sql_left += "resource_type, "
+				sql_left += "description, "
+				sql_left += "is_deprecated, "
+				sql_left += "hide_info, "
+				sql_left += "text, "
+				sql_left += "resource_file, "
+				sql_left += "grade_level, "
+				sql_left += "intended_use, "
+				sql_left += "license, "
+				sql_left += "license_link, "
+				sql_left += "license_other_notes, "
+				sql_left += "source, "
+				sql_left += "language, "
+				sql_left += "author, "
+				sql_left += "comments, "
+				sql_left += "creation_date, "
+				sql_left += "problem_type, "
+				sql_left += "solutions_hints_etc) "
+			
+				sql_middle = "= ('"
+			
+				sql_right = re.escape(name) + "', '" 
+				sql_right += resource_type  + "', '" 
+				sql_right += re.escape(description)  + "', '" 
+				sql_right += is_deprecated  + "', '" 
+				sql_right += hide_info  + "', '" 
+				sql_right += re.escape(text)  + "', '"
+				sql_right += resource_file  + "', '"
+				sql_right += grade_level  + "', '"
+				sql_right += intended_use  + "', '"
+				sql_right += re.escape(license)  + "', '"
+				sql_right += re.escape(license_link)  + "', '"
+				sql_right += re.escape(license_other_notes)  + "', '"
+				sql_right += source  + "', '"
+				sql_right += language  + "', '"
+				sql_right += re.escape(author)  + "', '"
+				sql_right += re.escape(comments)  + "', '"
+				sql_right += creation_date  + "', '"
+				sql_right += problem_type  + "', '"
+				sql_right += re.escape(solutions_hints_etc)  + "')"
+
+				sql_query = sql_start + sql_left + sql_middle + sql_right
+
+				# If this exact resource already exists, skip it.
+				if cur.execute(sql_query):
+					print "Skipping duplicate entry " + name
+
+				# If it's not a duplicate entry:
+				else:
+
+					# Run an "INSERT" command to put in this resource
+					sql_start = "INSERT RDB_resource "
+					sql_middle = "VALUES ('" 
+
+					sql_query = sql_start + sql_left + sql_middle + sql_right
+					cur.execute(sql_query)
+					added_resources += 1
+
+					# Get the ID of the resource I just created
+					resource_id = cur.lastrowid
+
+					# Link this resource to the current_collection, creating the collection if necessary.
+
+					################
+					# Automated Collection Creation!
+					# The Collection_Creator function returns the number of new collections that were added. Add 'em up.
+					###############
+
+					added_collections += Collection_Creator(collection_list, cur, resource_id)
+
+
+				"""
+							
+				
 		# Close the file - should be done automatically using the "with open" approach.
 
 	except IOError:
@@ -250,57 +351,61 @@ def FixPath(filepath, line):
 
 ####################################################
 # Automated Collection Creation!
+# Now with Multiple Collections!
 ####################################################
 
-def Collection_Creator(collection, cur, resource_id):
+def Collection_Creator(collection_list, cur, resource_id):
 
 	added_collections = 0
 
-	# Check to see if a collection with this name already exists. 
-	cur.execute("SELECT id FROM RDB_collection WHERE name = %s", collection)
+	# 
+	for collection in collection_list:
 
-	try:
-		# Set the collection id to that.
-		collection_id = cur.fetchone()[0]
-	except TypeError:
-		# Unless it's null or something, in which case set it to zero.
-		collection_id = 0
+		# Check to see if a collection with this name already exists. 
+		cur.execute("SELECT id FROM RDB_collection WHERE name = %s", collection)
 
-	# If not, create the collection.
-	if not collection_id:
+		try:
+			# Set the collection id to that.
+			collection_id = cur.fetchone()[0]
+		except TypeError:
+			# Unless it's null or something, in which case set it to zero.
+			collection_id = 0
 
-		# A big INSERT command to create the collection.
-		collection_query = "INSERT INTO RDB_collection "
+		# If not, create the collection.
+		if not collection_id:
 
-		collection_query += "(name, "
-		collection_query += "collection_type, "
-		collection_query += "is_sequential, "
-		collection_query += "is_deprecated, "
-		collection_query += "creation_date) "
+			# A big INSERT command to create the collection.
+			collection_query = "INSERT INTO RDB_collection "
 
-		collection_query += "VALUES ('"
+			collection_query += "(name, "
+			collection_query += "collection_type, "
+			collection_query += "is_sequential, "
+			collection_query += "is_deprecated, "
+			collection_query += "creation_date) "
 
-		collection_query += re.escape(collection) + "', '" 
-		collection_query += "other" + "', '" 
-		collection_query += "0"  + "', '" # is_sequential
-		collection_query += "0"  + "', '" # is_deprecated
-		collection_query += "2001-01-01" + "')" # creation_date
+			collection_query += "VALUES ('"
 
-		cur.execute(collection_query)
-		added_collections += 1
+			collection_query += re.escape(collection) + "', '" 
+			collection_query += "other" + "', '" 
+			collection_query += "0"  + "', '" # is_sequential
+			collection_query += "0"  + "', '" # is_deprecated
+			collection_query += "2001-01-01" + "')" # creation_date
+
+			cur.execute(collection_query)
+			added_collections += 1
 	
-		# Get the ID of the collection I just created.
-		collection_id = cur.lastrowid
+			# Get the ID of the collection I just created.
+			collection_id = cur.lastrowid
 	
-	# Add this resource to the collection.
-	resource_insert_query = "INSERT INTO RDB_collection_included_resources "
-	resource_insert_query += "(collection_id, "
-	resource_insert_query += "resource_id) "
-	resource_insert_query += "VALUES ('"
-	resource_insert_query += str(collection_id) + "', '"
-	resource_insert_query += str(resource_id) + "')"
+		# Add this resource to the collection.
+		resource_insert_query = "INSERT INTO RDB_collection_included_resources "
+		resource_insert_query += "(collection_id, "
+		resource_insert_query += "resource_id) "
+		resource_insert_query += "VALUES ('"
+		resource_insert_query += str(collection_id) + "', '"
+		resource_insert_query += str(resource_id) + "')"
 
-	cur.execute(resource_insert_query)
+		cur.execute(resource_insert_query)
 
 	return added_collections
 
