b7f574fae7e187712ecbe16b5cf81e3c9e664e79
Colin Fredericks <cfred@mit.edu>
2013-08-16 13:46:28 -0400

    More work-in-progress

--- utils/toy_course/new_read_in_course.py
+++ utils/toy_course/new_read_in_course.py
@@ -25,7 +25,9 @@ from collections import OrderedDict		# Ordered dictionary
 from lxml import etree					# XML parser
 
 
-# Main takes in arguments, connects to the database, and runs BigLoop.
+####################################################
+# Main takes in arguments, connects to the database, and runs TheOpener.
+####################################################
 def main(argv):
 
 	# Check for correct usage
@@ -61,7 +63,9 @@ def main(argv):
 
 
 
-# This one opens files and sends them to the XML processer.
+####################################################
+# Reads in files and sends them to the XML Processor or the Resource Muncher
+####################################################
 def TheOpener(filepath, tag_type, display_name, containers, depth, cur, db):
 
 	# try to open the file, to double-check filepath
@@ -89,13 +93,38 @@ def TheOpener(filepath, tag_type, display_name, containers, depth, cur, db):
 	try:
 		with open(filepath, 'rbU') as xmlfile:
 
+			# Use the first tag in the file to double-check the display_name. (But not for the first file.)
+			if depth > 0:
+		
+				if root.get('display_name'):
+		
+					# If the display name doesn't match the current display_name variable, update the variable.
+					d_n = root.get('display_name')
+					if d_n:
+						if d_n != display_name:
+							display_name = d_n
+							# Remove the existing one if we're going to replace it.
+							containers.popitem()
+							AddWithoutDuplicates(containers, display_name, root.tag)
+
+				# If an "unknown" display name was passed, and we can't find one here, this file's name should be its actual filename.
+				elif 'unknown' in display_name:
+					display_name = os.path.basename(xmlfile.name)
+					# Remove the existing one if we're going to replace it.
+					containers.popitem()
+					AddWithoutDuplicates(containers, display_name, root.tag)
+
 			print 'New file: ' + filepath
 			
 			# Get the text from this file.
 			xmltext = xmlfile.read()
 			
-			# Send it to the XML processor.
-			BigLoop(xmltext, filepath, tag_type, display_name, containers, depth, cur, db)
+			# If this file is a resource, send it to the Resource Muncher.
+			# Otherwise, it's a container. Send it to the XML processor. 
+			if 'html' in tag_type or 'problem' in tag_type:
+				ResourceMuncher(xmltext, filepath, tag_type, display_name, containers, depth, cur, db)
+			else:
+				XMLProcessor(xmltext, filepath, tag_type, display_name, containers, depth, cur, db)
 
 		# Close file - done automatically via "with"
 	
@@ -106,15 +135,10 @@ def TheOpener(filepath, tag_type, display_name, containers, depth, cur, db):
 		containers.popitem()
 
 
+####################################################
 # This does the work of examining the XML and traversing it.
-def BigLoop(xmltext, filepath, tag_type, display_name, containers, depth, cur, db):
-
-	# Various trackers
-	# linked_objectives = 0 
-	# added_resources = 0
-	filepaths_found = 0
-	added_collections = 0
-
+####################################################
+def XMLProcessor(xmltext, filepath, tag_type, display_name, containers, depth, cur, db):
 
 	# Keep track of how deep we are so we can exit if we're stuck in a loop.
 	depth += 1
@@ -125,46 +149,25 @@ def BigLoop(xmltext, filepath, tag_type, display_name, containers, depth, cur, d
 	root = etree.fromstring(xmltext)
 	# print etree.tostring(root, pretty_print=True)
 	
-	# Use the first tag in the file to double-check the display_name. (But not for the first file.)
-	if depth > 0:
-		
-		if root.get('display_name'):
-		
-			# If the display name doesn't match the current display_name variable, update the variable.
-			d_n = root.get('display_name')
-			if d_n:
-				if d_n != display_name:
-					display_name = d_n
-					# Remove the existing one if we're going to replace it.
-					containers.popitem()
-					AddWithoutDuplicates(containers, display_name, root.tag)
-
-		# If an "unknown" display name was passed, and we can't find one here, this file's name should be its actual filename.
-		elif 'unknown' in display_name:
-			display_name = os.path.basename(xmlfile.name)
-			# Remove the existing one if we're going to replace it.
-			containers.popitem()
-			AddWithoutDuplicates(containers, display_name, root.tag)
-
 	# For every tag in this XML tree:
 	for x in root.iter():
 	
 		# Skip comments.
 		if x.tag is not etree.Comment:
 
-			# If this tag is a container:
+			# If this is the kind of tag you built edX courses from...
 			if 'course' in x.tag \
 				or 'chapter' in x.tag \
 				or 'sequential' in x.tag \
-				or 'vertical'  in x.tag \
-				or 'conditional'  in x.tag \
-				or 'html'  in x.tag \
-				or 'problem' in x.tag:
+				or 'vertical' in x.tag \
+				or 'conditional' in x.tag \
+				or 'problem' in x.tag \
+				or 'html' in x.tag:
 		
 				print x.tag
 	
-				# If it's an inline container, get its info and add it to the container list.
-				# Then run this loop with the text inside the container.
+				# If it's an inline definition, get its info and add it to the container list.
+				# Then run this function again with the text inside the container.
 				if x.text:
 					print '...is an inline container'
 					# An inline container has stuff other than whitespace inside the tag.
@@ -175,12 +178,17 @@ def BigLoop(xmltext, filepath, tag_type, display_name, containers, depth, cur, d
 							display_name = x.get('display_nme')
 						else:
 							display_name = 'unknown' + x.tag
-
+						
+						# If this is an HTML snippet or a problem defined inline,
+						# skip it.
+						if 'html' in x.tag or 'problem' in x.tag:
+							print 'Skipping inline ' + x.tag + ' item in file  ' +  + '.'
+						
 						# Add this item to the container dictionary.
 						AddWithoutDuplicates(containers, display_name, x.tag)
 						
 						# Run this routine on the text inside the tag as if it were a file.
-						BigLoop(x.text, filepath, x.tag, display_name, containers, depth, cur, db)
+						XMLProcessor(x.text, filepath, x.tag, display_name, containers, depth, cur, db)
 			
 				# If it's a self-closing container (no stuff in tag), attempt to follow the file it links to.
 				# We're recursively traversing the file tree.
@@ -237,155 +245,149 @@ def BigLoop(xmltext, filepath, tag_type, display_name, containers, depth, cur, d
 						# Recursion happens here, but we need to open the file first.
 						TheOpener(filepath, x.tag, display_name, containers, depth, cur, db)
 						db.commit()
-			
-						filepaths_found += 1						
 
 	# Move to next tag (done automatically by the for loop)
 
 
-def ResourceMuncher()
-	# If there are no self-closing tags with filepaths found in this whole file:
-	if filepaths_found == 0:
+####################################################
+# This takes in resources and adds them to the database.
+####################################################
+def ResourceMuncher(xmltext, filepath, tag_type, display_name, containers, depth, cur, db):
 
-		# This page is a resource. Remove its name from the collection list.
-		containers.popitem()
+	# This page is a resource. Remove its name from the collection list.
+	containers.popitem()
 
-		# We're going to INSERT a new resource into the database.
+	# We're going to INSERT a new resource into the database.
 
-		# Use the display_name that was passed (as ammended above) to name this resource.
-		# If that name is blank, use the filename.
-		if not display_name:
-			display_name = os.path.basename(xmlfile.name)
-		name = display_name
+	# Use the display_name that was passed (as ammended above) to name this resource.
+	# If that name is blank, use the filename.
+	if not display_name:
+		display_name = os.path.basename(xmlfile.name)
+	name = display_name
 
-		# Use the tag type to set the resource_type to html or problem.
-		if tag_type == 'html':
-			resource_type = 'html'
-		elif tag_type == 'problem':
-			resource_type = 'problem'
+	# Use the tag type to set the resource_type to html or problem.
+	if tag_type == 'html':
+		resource_type = 'html'
+	elif tag_type == 'problem':
+		resource_type = 'problem'
+	else:
+		resource_type = 'other'
+		# (What to do with videos?)
+
+	# Take the entire text of this file and dump it into the "text" field.
+	text = xmltext
+
+	# If the resource type is problem:
+	if resource_type == 'problem':
+	
+		# Use regex to set problem_type based on whether it's <multiplechoice>, <numericresponse>, <formularesponse>, etc.
+		if re.search('<multiplechoice', text):
+			problem_type = "multiple_choice"
+		elif re.search('<numericalresponse', text):
+			problem_type = 'numerical'
+		elif re.search('<formularesponse', text):
+			problem_type = 'formula'
+		elif re.search('<symbolicresponse', text):
+			problem_type = 'formula'
+		elif re.search('<choiceresponse', text):
+			problem_type = 'select_all'
+		elif re.search('<custom', text):
+			problem_type = 'custom'
 		else:
-			resource_type = 'other'
-			# (What to do with videos?)
+			# If none of these tags are found, go back and set the resource_type to "other"
+			problem_type = 'other'
+	else:
+		problem_type = 'not_a_problem'
 
-		# Take the entire text of this file and dump it into the "text" field.
-		with open(filepath, 'rbU') as tempfile:
-			text = tempfile.read()
-
-		# If the resource type is problem:
-		if resource_type == 'problem':
-		
-			# Use regex to set problem_type based on whether it's <multiplechoice>, <numericresponse>, <formularesponse>, etc.
-			if re.search('<multiplechoice',text):
-				problem_type = "multiple_choice"
-			elif re.search('<numericalresponse',text):
-				problem_type = 'numerical'
-			elif re.search('<formularesponse', text):
-				problem_type = 'formula'
-			elif re.search('<symbolicresponse', text):
-				problem_type = 'formula'
-			elif re.search('<choiceresponse', text):
-				problem_type = 'select_all'
-			elif re.search('<custom', text):
-				problem_type = 'custom'
-			else:
-				# If none of these tags are found, go back and set the resource_type to "other"
-				problem_type = 'other'
-		else:
-			problem_type = 'not_a_problem'
-
-		# Placeholder values.
-		# Note that we'll need to link learning objectives later.
-		description = ''
-		is_deprecated = '0'		# 0 for false in SQL
-		hide_info = '0'			# 0 for false in SQL
-		resource_file = ''
-		grade_level = ''
-		intended_use = ''
-		license = ''
-		license_link = ''
-		license_other_notes = ''
-		source = ''
-		language = ''
-		author = ''
-		comments = ''
-		creation_date = '2001-01-01'
-		solutions_hints_etc = ''
+	# Placeholder values.
+	# Note that we'll need to link learning objectives later.
+	description = ''
+	is_deprecated = '0'		# 0 for false in SQL
+	hide_info = '0'			# 0 for false in SQL
+	resource_file = ''
+	grade_level = ''
+	intended_use = ''
+	license = ''
+	license_link = ''
+	license_other_notes = ''
+	source = ''
+	language = ''
+	author = ''
+	comments = ''
+	creation_date = '2001-01-01'
+	solutions_hints_etc = ''
 
 
-		# Assemble the MySQL INSERT command.
-		sql_start = "SELECT * FROM RDB_resource  WHERE"
-	
-		sql_left = "(name, "
-		sql_left += "resource_type, "
-		sql_left += "description, "
-		sql_left += "is_deprecated, "
-		sql_left += "hide_info, "
-		sql_left += "text, "
-		sql_left += "resource_file, "
-		sql_left += "grade_level, "
-		sql_left += "intended_use, "
-		sql_left += "license, "
-		sql_left += "license_link, "
-		sql_left += "license_other_notes, "
-		sql_left += "source, "
-		sql_left += "language, "
-		sql_left += "author, "
-		sql_left += "comments, "
-		sql_left += "creation_date, "
-		sql_left += "problem_type, "
-		sql_left += "solutions_hints_etc) "
-	
-		sql_middle = "= ('"
-	
-		sql_right = re.escape(name) + "', '" 
-		sql_right += resource_type  + "', '" 
-		sql_right += re.escape(description)  + "', '" 
-		sql_right += is_deprecated  + "', '" 
-		sql_right += hide_info  + "', '" 
-		sql_right += re.escape(text)  + "', '"
-		sql_right += resource_file  + "', '"
-		sql_right += grade_level  + "', '"
-		sql_right += intended_use  + "', '"
-		sql_right += re.escape(license)  + "', '"
-		sql_right += re.escape(license_link)  + "', '"
-		sql_right += re.escape(license_other_notes)  + "', '"
-		sql_right += source  + "', '"
-		sql_right += language  + "', '"
-		sql_right += re.escape(author)  + "', '"
-		sql_right += re.escape(comments)  + "', '"
-		sql_right += creation_date  + "', '"
-		sql_right += problem_type  + "', '"
-		sql_right += re.escape(solutions_hints_etc)  + "')"
+	# Assemble the MySQL INSERT command.
+	sql_start = "SELECT * FROM RDB_resource  WHERE"
+
+	sql_left = "(name, "
+	sql_left += "resource_type, "
+	sql_left += "description, "
+	sql_left += "is_deprecated, "
+	sql_left += "hide_info, "
+	sql_left += "text, "
+	sql_left += "resource_file, "
+	sql_left += "grade_level, "
+	sql_left += "intended_use, "
+	sql_left += "license, "
+	sql_left += "license_link, "
+	sql_left += "license_other_notes, "
+	sql_left += "source, "
+	sql_left += "language, "
+	sql_left += "author, "
+	sql_left += "comments, "
+	sql_left += "creation_date, "
+	sql_left += "problem_type, "
+	sql_left += "solutions_hints_etc) "
+
+	sql_middle = "= ('"
+
+	sql_right = re.escape(name) + "', '" 
+	sql_right += resource_type  + "', '" 
+	sql_right += re.escape(description)  + "', '" 
+	sql_right += is_deprecated  + "', '" 
+	sql_right += hide_info  + "', '" 
+	sql_right += re.escape(text)  + "', '"
+	sql_right += resource_file  + "', '"
+	sql_right += grade_level  + "', '"
+	sql_right += intended_use  + "', '"
+	sql_right += re.escape(license)  + "', '"
+	sql_right += re.escape(license_link)  + "', '"
+	sql_right += re.escape(license_other_notes)  + "', '"
+	sql_right += source  + "', '"
+	sql_right += language  + "', '"
+	sql_right += re.escape(author)  + "', '"
+	sql_right += re.escape(comments)  + "', '"
+	sql_right += creation_date  + "', '"
+	sql_right += problem_type  + "', '"
+	sql_right += re.escape(solutions_hints_etc)  + "')"
+
+	sql_query = sql_start + sql_left + sql_middle + sql_right
+
+	# If this exact resource already exists, skip it.
+	if cur.execute(sql_query):
+		print "Skipping duplicate entry " + name
+
+	# If it's not a duplicate entry:
+	else:
+
+		# Run an "INSERT" command to put in this resource
+		sql_start = "INSERT RDB_resource "
+		sql_middle = "VALUES ('" 
 
 		sql_query = sql_start + sql_left + sql_middle + sql_right
 
-		# If this exact resource already exists, skip it.
-		if cur.execute(sql_query):
-			print "Skipping duplicate entry " + name
+		cur.execute(sql_query)
 
-		# If it's not a duplicate entry:
-		else:
+		# Get the ID of the resource I just created
+		resource_id = cur.lastrowid
 
-			# Run an "INSERT" command to put in this resource
-			sql_start = "INSERT RDB_resource "
-			sql_middle = "VALUES ('" 
+		# Link this resource to the current_collection, creating the collection if necessary.
+		Collection_Creator(containers, cur, resource_id)
 
-			sql_query = sql_start + sql_left + sql_middle + sql_right
-
-			cur.execute(sql_query)
-
-			# Get the ID of the resource I just created
-			resource_id = cur.lastrowid
-
-			# Link this resource to the current_collection, creating the collection if necessary.
-
-			################
-			# Automated Collection Creation!
-			# The Collection_Creator function returns the number of new collections that were added. Add 'em up.
-			###############
-
-			added_collections += Collection_Creator(containers, cur, resource_id)
-			db.commit()
+		# Commit the database changes.
+		db.commit()
 
 
 ####################################################
